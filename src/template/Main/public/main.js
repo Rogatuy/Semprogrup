f22a8cf2aa3912578a4e2fa34850a22ced1691e2
Rogatuy <evdemonist773@gmail.com>
2024-08-20 14:15:22 +0500

    asd

--- src/template/Main/public/main.js
+++ src/template/Main/public/main.js
@@ -4,3 +4,15 @@ import './../../../app/js/modalAction.js';
 import './../../../components/Shared/Popups/order-call-popup/order-call.js';
 import './../../../components/Shared/Popups/video-popup/video.js';
 //? Chapter Unique Components
+
+import fullpage from 'fullpage.js';
+
+document.addEventListener('DOMContentLoaded', function () {
+  new fullpage('#fullpage', {
+    autoScrolling: false, // Отключает авто-прокрутку
+    fitToSection: false, // Отключает автоматическое подстраивание секции под экран
+    scrollBar: true, // Включает стандартный скролл
+    navigation: true, // Можно оставить для навигации
+    navigationPosition: 'right',
+  });
+});
